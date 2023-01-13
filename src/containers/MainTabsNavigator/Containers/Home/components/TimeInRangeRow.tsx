78e95098d1a44515b9fbefcf531e50d15a3040f4
Irad Kotton <irad.kotton@dreamed.ai>
2023-01-13 23:01:30 +0200

    Added animation for TimeInRangeRow

--- src/containers/MainTabsNavigator/Containers/Home/components/TimeInRangeRow.tsx
+++ src/containers/MainTabsNavigator/Containers/Home/components/TimeInRangeRow.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styled from 'styled-components/native';
 import {BgSample} from 'app/types/day_bgs';
 import {Animated, Text} from 'react-native';
@@ -18,7 +18,7 @@ const Container = styled.View`
   border: 2px solid #333;
 `;
 
-const Column = styled.View<{color: string; width: number}>`
+const Column = styled(Animated.View)<{color: string; width: number}>`
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -39,6 +39,26 @@ interface TimeInRangeRowProps {
 }
 
 export const TimeInRangeRow: React.FC<TimeInRangeRowProps> = ({bgData}) => {
+  const animatedLowPercentage = useRef(new Animated.Value(0)).current;
+  const animatedHighPercentage = useRef(new Animated.Value(0)).current;
+  const animatedInTargetPercentage = useRef(new Animated.Value(0)).current;
+
+  interface AnimationValues {
+    lowPercentageAnimation: number;
+    highPercentageAnimation: number;
+    inTargetPercentageAnimation: number;
+  }
+
+  const [animationValues, setAnimationValues] = useState<AnimationValues>({
+    lowPercentageAnimation: 0,
+    highPercentageAnimation: 0,
+    inTargetPercentageAnimation: 0,
+  });
+  const {
+    lowPercentageAnimation,
+    highPercentageAnimation,
+    inTargetPercentageAnimation,
+  } = animationValues;
   const timeInRange = bgData.filter(
     // @ts-ignore
     bg => bg.sgv >= cgmRange.TARGET.min && bg.sgv <= cgmRange.TARGET.max,
@@ -57,16 +77,60 @@ export const TimeInRangeRow: React.FC<TimeInRangeRowProps> = ({bgData}) => {
       100,
   );
 
+  useEffect(() => {
+    const runAnimationTimeout = setTimeout(() => {
+      const animation = new Animated.Value(0);
+      const inputRange = [0, 100];
+      const outputRange = ['0%', '100%'];
+      Animated.timing(animatedLowPercentage, {
+        toValue: lowPercentage || 0,
+        duration: 500,
+        useNativeDriver: false,
+      }).start();
+      Animated.timing(animatedHighPercentage, {
+        toValue: highPercentage || 0,
+        duration: 500,
+        useNativeDriver: false,
+      }).start();
+      Animated.timing(animatedInTargetPercentage, {
+        toValue: timeInRangePercentage || 0,
+        duration: 500,
+        useNativeDriver: false,
+      }).start();
+
+      const lowPercentageAnimation = animatedLowPercentage.interpolate({
+        inputRange,
+        outputRange,
+      });
+      const highPercentageAnimation = animatedHighPercentage.interpolate({
+        inputRange,
+        outputRange,
+      });
+      const inTargetPercentageAnimation =
+        animatedInTargetPercentage.interpolate({
+          inputRange,
+          outputRange,
+        });
+
+      setAnimationValues({
+        lowPercentageAnimation: lowPercentageAnimation,
+        highPercentageAnimation: highPercentageAnimation,
+        inTargetPercentageAnimation: inTargetPercentageAnimation,
+      });
+    }, 200);
+    return () => clearTimeout(runAnimationTimeout);
+  }, [bgData]);
+
   return (
     <Container>
-      <Column width={lowPercentage} color={'red'}>
-        <PercentageText>{lowPercentage}%</PercentageText>
+      <Column style={{width: lowPercentageAnimation}} color={'red'}>
+        <PercentageText>{lowPercentage || 0}%</PercentageText>
       </Column>
-      <Column width={timeInRangePercentage} color={'green'}>
-        <PercentageText>{timeInRangePercentage}%</PercentageText>
+      <Column style={{width: inTargetPercentageAnimation}} color={'green'}>
+        <PercentageText>{timeInRangePercentage || 0}%</PercentageText>
       </Column>
-      <Column width={highPercentage} color={'yellow'}>
-        <PercentageText color={'black'}>{highPercentage}%</PercentageText>
+      <Column style={{width: highPercentageAnimation}} color={'yellow'}>
+        <PercentageText color={'black'}>{highPercentage || 0}%</PercentageText>
       </Column>
     </Container>
   );
