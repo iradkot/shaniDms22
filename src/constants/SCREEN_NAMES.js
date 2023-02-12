5eaabe4a15f9ec0fd4d6cea2bba3e5973db02212
Irad Kotton <irad.kotton@dreamed.ai>
2023-02-12 04:00:37 +0200

    Change screen names to have types

--- src/constants/SCREEN_NAMES.js
+++
@@ -1,7 +0,0 @@
-export const MAIN_TAB_NAVIGATOR = 'HomeScreen';
-export const HOME_TAB_SCREEN = 'HomeTabScreen';
-export const NOTIFICATION_TAB_SCREEN = 'NotificationTabScreen';
-export const Food_Tracking_TAB_SCREEN = 'FoodTrackingTabScreen';
-export const LOGIN_SCREEN = 'LoginScreen';
-export const ADD_NOTIFICATION_SCREEN = 'AddNotificationScreen';
-export const EDIT_NOTIFICATION_SCREEN = 'EditNotificationScreen';
---
+++ src/constants/SCREEN_NAMES.ts
@@ -0,0 +1,9 @@
+export const MAIN_TAB_NAVIGATOR: string = 'HomeScreen';
+export const HOME_TAB_SCREEN: string = 'HomeTabScreen';
+export const NOTIFICATION_TAB_SCREEN: string = 'NotificationTabScreen';
+export const Food_Tracking_TAB_SCREEN: string = 'FoodTrackingTabScreen';
+export const LOGIN_SCREEN: string = 'LoginScreen';
+export const ADD_NOTIFICATION_SCREEN: string = 'AddNotificationScreen';
+export const EDIT_NOTIFICATION_SCREEN: string = 'EditNotificationScreen';
+export const FOOD_ITEM_TAB_SCREEN: string = 'FoodItemTabScreen';
+export const CAMERA_SCREEN: string = 'CameraScreen';
