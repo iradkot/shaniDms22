a3273df1229034baa34222f2e5c551b30a8f58c5
iradkot <iradkot@users.noreply.github.com>
2026-04-11 23:28:29 +0300

    fix(home): always show live glucose header on all dates

--- src/containers/MainTabsNavigator/Containers/Home/sections/HomeHeaderSection.tsx
+++ src/containers/MainTabsNavigator/Containers/Home/sections/HomeHeaderSection.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import TimeInRangeRow from 'app/containers/MainTabsNavigator/Containers/Home/components/TimeInRangeRow';
-import LatestCgmRow from 'app/containers/MainTabsNavigator/Containers/Home/components/LatestCgmRow';
 import SmartExpandableHeader from 'app/containers/MainTabsNavigator/Containers/Home/components/SmartExpandableHeader';
 import type {BgSample} from 'app/types/day_bgs.types';
 
@@ -24,35 +23,22 @@ type Props = {
 
 export const HomeHeaderSection: React.FC<Props> = ({
   bgData,
-  isShowingToday,
   headerLatestBgSample,
   headerLatestPrevBgSample,
-  latestBgSample,
-  latestPrevBgSample,
-  listBgData,
   maxIobReference,
   maxCobReference,
-  onRefreshBgData,
 }) => {
   return (
     <>
       <TimeInRangeRow bgData={bgData} />
 
-      {isShowingToday ? (
-        <SmartExpandableHeader
-          fallbackLatestBgSample={headerLatestBgSample ?? undefined}
-          latestPrevBgSample={headerLatestPrevBgSample ?? undefined}
-          maxIobReference={maxIobReference}
-          maxCobReference={maxCobReference}
-        />
-      ) : (
-        <LatestCgmRow
-          latestPrevBgSample={latestPrevBgSample}
-          latestBgSample={latestBgSample}
-          allBgData={listBgData}
-          onRefresh={onRefreshBgData}
-        />
-      )}
+      {/* Always show the live/current header, even when browsing past dates. */}
+      <SmartExpandableHeader
+        fallbackLatestBgSample={headerLatestBgSample ?? undefined}
+        latestPrevBgSample={headerLatestPrevBgSample ?? undefined}
+        maxIobReference={maxIobReference}
+        maxCobReference={maxCobReference}
+      />
     </>
   );
 };
